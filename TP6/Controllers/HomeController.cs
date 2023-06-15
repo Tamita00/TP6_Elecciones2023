76a5e6282ffe952ebf9ee069b4b1e67750b01cf8
TAMARA YAEL DRATLER <47807159@ort>
2023-06-15 09:04:16 -0300

    hola3!

--- TP6/Controllers/HomeController.cs
+++ TP6/Controllers/HomeController.cs
@@ -5,7 +5,43 @@ namespace TP6.Controllers;
 public class HomeController : Controller
 {
     public IActionResult Index()
-    {
+    { 
+        ViewBag.listaPartidos = BD.ListarPartidos();
         return View();
+    }
+    public IActionResult VerDetallePartido(int idPartido)
+    { 
+        ViewBag.detallePArtido = BD.VerInfoPartido(idPartido);
+        return View("DetallePartido");
+    }
+    public IActionResult VerDetalleCandidato(int idCandidato)
+    { 
+        ViewBag.detalleCandidato = BD.VerInfoCandidato(idCandidato);
+        return View("DetalleCandidato");
+    }
+    public IActionResult AgregarCandidato(int idPartido)
+    { 
+        ViewBag.idPartido = idPartido;
+        return View("CargarCandidato");
+    }
+
+    [HttpPost]
+    public IActionResult GuardarCandidato(Candidato candidato)
+    { 
+        BD.AgregarCandidato(candidato);
+        return RedirectToAction("VerDetallePartido", new { idPartido = candidato.IdPartido});
+    }
+    public IActionResult EliminarCandidato(int idCandidato, int IdPartido)
+    { 
+        BD.EliminarCandidato(idCandidato);
+        return RedirectToAction("VerDetallePartido", new { dPartido = IdPartido});
+    }
+    public IActionResult Elecciones()
+    { 
+        return View("Elecciones");
+    }
+    public IActionResult Creditos()
+    { 
+        return View("Creditos");
     }
 }
